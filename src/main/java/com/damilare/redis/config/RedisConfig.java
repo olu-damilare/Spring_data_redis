c15ab0d4f892570a62a530129b98c74c4cdfad4e
Damilare Jolayemi <djolayemi@gmail.com>
2022-04-15 21:42:26 +0100

    heroku updates

--- src/main/java/com/damilare/redis/config/RedisConfig.java
+++ src/main/java/com/damilare/redis/config/RedisConfig.java
@@ -1,11 +1,6 @@
 package com.damilare.redis.config;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
